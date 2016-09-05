090f0fc74a336719639f63485a9ea45435b63440
Xomer89 <vpannochka@yandex.ru>
2016-09-05 16:11:50 +0300

    critter edit

--- untitled/OOP/Critter.py
+++ untitled/OOP/Critter.py
@@ -1,11 +1,17 @@
-#A simple class presentaion
+# A simple class presentaion
 
 class Critter(object):
     """Virtual pet"""
+
+    def __init__(self):
+        print("A new pet created")
+
     def talk(self):
         print("Hey! I'm a pet from class Critter")
 
 
-crit = Critter()
-crit.talk()
+crit1 = Critter()
+crit2 = Critter()
+crit1.talk()
+crit2.talk()
 input("\n\nPress ENTER to exit.")
