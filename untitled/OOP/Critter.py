ef6604c22bf695b6dc75cba10e5116cf2ac00294
Xomer89 <vpannochka@yandex.ru>
2016-09-05 16:21:09 +0300

    critter edit

--- untitled/OOP/Critter.py
+++ untitled/OOP/Critter.py
@@ -3,15 +3,25 @@
 class Critter(object):
     """Virtual pet"""
 
-    def __init__(self):
+    def __init__(self, name):
         print("A new pet created")
+        self.name = name
+
+    def __str__(self):
+        rep = "Object from class Critter\n"
+        rep += "Name: " + self.name + "\n"
+        return rep
 
     def talk(self):
         print("Hey! I'm a pet from class Critter")
 
 
-crit1 = Critter()
-crit2 = Critter()
+crit1 = Critter("Milo")
 crit1.talk()
+crit2 = Critter("Pluto")
 crit2.talk()
+print("Printing crit1: ")
+print(crit1)
+print("Access to crit1.name: ")
+print(crit1.name)
 input("\n\nPress ENTER to exit.")
