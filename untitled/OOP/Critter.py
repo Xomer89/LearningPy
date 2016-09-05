4c4a2b139b431992371830d5359c9208f9c39be4
Xomer89 <vpannochka@yandex.ru>
2016-09-05 11:21:52 +0300

    Added a directory

---
+++ untitled/OOP/Critter.py
@@ -0,0 +1,10 @@
+#A simple class presentaion
+
+class Critter(object):
+    def talk(self):
+        print("Hey! I'm a pet from class Critter")
+
+
+crit = Critter()
+crit.talk()
+input("\n\nPress ENTER to exit.")
