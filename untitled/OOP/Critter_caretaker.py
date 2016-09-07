66b3daf08c972348717c3b139452eee0476134ba
Xomer89 <vpannochka@yandex.ru>
2016-09-07 11:06:30 +0300

    critter_caretaker edit

--- untitled/OOP/Critter_caretaker.py
+++ untitled/OOP/Critter_caretaker.py
@@ -30,8 +30,50 @@ class Critter(object):
 
     def eat(self, food = 4):
         print("OM-NOM-NOM....Thanks")
-        self.hunger = food
+        self.hunger -= food
         if self.hunger < 0:
             self.hunger = 0
         self.__pass_time()
+
+    def play(self, fun = 4):
+        print("Wiiiiiiii!")
+        self.boredom -= fun
+        if self.boredom < 0:
+            self.boredom = 0
+        self.__pass_time()
+
+def main():
+    crit_name = input("What will you call your pet?")
+    crit = Critter(crit_name)
+
+    choice = None
+    while choice != "0":
+        #Menu
+        print("""
+        My Pet
+        1 - Ask how it feels
+        2 - Feed it
+        3 - Play with it
+        0 - Exit
+        """)
+        choice = input("Your choice")
+        print()
+
+        #Talk to pet
+        if choice == "1":
+            crit.talk()
+        #Feed pet
+        elif choice == "2":
+            crit.eat()
+        #Play with pet
+        elif choice == "3":
+            crit.play()
+        #Exit
+        elif choice == "0":
+            print("Bye-bye")
+        else:
+            print("There's no option", choice)
+
+main()
+input("\n\nPress ENTER to exit.")
 
